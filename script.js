f180955380decd559d8910b333b6ed9a4eb420fc
Jasmin Omanovic <civonamo@gmail.com>
2025-02-07 15:22:22 -0500

    fix: remove whatsapp rotation, switch to msg icon

--- script.js
+++
@@ -1,8 +0,0 @@
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    const whatsappFloat = document.querySelector('.whatsapp-float');
-    setInterval(() => {
-        whatsappFloat.classList.toggle('flipped');
-    }, 3000);
-});
